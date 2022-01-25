5171e10f95c1826aa1c314b23e13d51866f854ff
florence.aj <florence.aj@inapp.com>
2022-01-25 18:17:37 +0530

    Colors description

--- src/Components/title.js
+++ src/Components/title.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Title = () => {
   return <div className='title'>
       <h1>To Do App</h1>
-      <p>Pending tasks will be displayed yellow, in progress tasks will be indigo, completed tasks will be green</p>
+      <p style={{ color : 'blue'}}>Pending or new tasks will be displayed yellow, in progress tasks will be indigo & completed tasks will be green</p>
   </div>;
 };
 
